2fccd45eee1e6d76d0fde9b8ebde9861b7b8fada
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-05-16 14:33:13 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,19 @@
+import requests
+import importlib.util
+
+# URL of the raw .py file on GitHub
+github_url = ""
+
+# Fetching the content of the .py file
+response = requests.get(github_url)
+code = response.text
+
+# Executing the code to make the function available
+exec(code)
+
+# Now you can use the function
+sad_you()
+
 import streamlit as st 
 from hello_you import sad_you()
 st.title("Accha ji mai hara chaloo maaan jaoooo na")
