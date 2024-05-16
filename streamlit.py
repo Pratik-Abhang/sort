421180ded9161104210cf7974b65f7cd8b8a4772
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-05-16 14:00:19 +0530

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+st.title("Accha ji mai hara chaloo maaan jaoooo na")
+user_input = st.text_input("Enter here")
+if st.button("click me"):
+  st.write("sad_you(user_input)")
+
