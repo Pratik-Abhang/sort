37d3a89a62e79bc1073ae6c1a3833688671ba0ca
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-05-16 15:12:11 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,23 +2,23 @@ import streamlit as st
 
 def sad_you(ip):
     if ip == 'i love you':
-        print( "Well, I found a woman, stronger than anyone I know\n She shares my dreams, I hope that someday Ill share her home\n I found a lover, to carry more than just my secrets To carry love, to carry children of our own")
+        return "Well, I found a woman, stronger than anyone I know<br> She shares my dreams, I hope that someday Ill share her home<br> I found a lover, to carry more than just my secrets To carry love, to carry children of our own"
     elif ip == 'i hate you':
-        print( "We are still kids, but we're so in love Fighting against all odds\n I know we'll be alright this time\n Darling, just hold my hand Be my girl, I'll be your man\n I see my future in your eyes")
+        return "We are still kids, but we're so in love Fighting against all odds<br> I know we'll be alright this time<br> Darling, just hold my hand Be my girl, I'll be your man<br> I see my future in your eyes"
     elif ip == 'i am mad at you':
-        print( "we were just kids when we fell in love\n Not knowing what it was\n I will not give you up this time\n But darling, just kiss me slow\n Your heart is all I own\n And in your eyes, you're holding mine")
+        return "we were just kids when we fell in love<br> Not knowing what it was<br> I will not give you up this time<br> But darling, just kiss me slow<br> Your heart is all I own<br> And in your eyes, you're holding mine"
     elif ip == 'maska nko maru':
-        print( "I have faith in what I see\n Now I know I have met an angel in person\n And she looks perfect")
+        return "I have faith in what I see<br> Now I know I have met an angel in person<br> And she looks perfect"
     elif ip == 'its ok':
-        print( "I don't deserve this\nYou look perfect tonight")
+        return "I don't deserve this<br>You look perfect tonight"
     elif ip == 'mg':
-        print( "Baby, I'm dancing in the dark\n With you between my arms\n Barefoot on the grass\n Listening to our favourite song\n When you said you looked a mess\n I whispered underneath my breath\n But you heard it\n Darling, you look perfect tonight")
+        return "Baby, I'm dancing in the dark<br> With you between my arms<br> Barefoot on the grass<br> Listening to our favourite song<br> When you said you looked a mess<br> I whispered underneath my breath<br> But you heard it<br> Darling, you look perfect tonight"
     elif ip == 'i dont want to talk':
-        print( "I found a love, for me\n Darling, just dive right in and follow my lead\n Well, I found a girl, beautiful and sweet\n Oh, I never knew you were the someone waiting for me\n")
+        return "I found a love, for me<br> Darling, just dive right in and follow my lead<br> Well, I found a girl, beautiful and sweet<br> Oh, I never knew you were the someone waiting for me"
     else:
-        print( "I have faith in what I see\n Now I know I have met an angel in person\n And she looks perfect\n I don't deserve this\nYou look perfect tonight")
+        return "I have faith in what I see<br> Now I know I have met an angel in person<br> And she looks perfect<br> I don't deserve this<br>You look perfect tonight"
 
 st.title("hello")
 ip = st.text_input("Enter here")
 if st.button("click me"):
-    st.markdown(sad_you(ip))
+    st.markdown(sad_you(ip), unsafe_allow_html=True)
